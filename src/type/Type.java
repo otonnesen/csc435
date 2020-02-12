7e313d7f630834cbb7509701c3b249739f8b2e0d
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-11 21:39:59 -0800

    Added equals() and comparable() methods to Type class.

--- src/type/Type.java
+++ src/type/Type.java
@@ -4,4 +4,12 @@ public abstract class Type {
 	private static String name;
 
 	public abstract String toString();
+
+	public boolean equals(Type other) {
+		return this.getClass().equals(other.getClass());
+	}
+
+	public boolean comparable(Type other) {
+		return this.getClass().isAssignableFrom(other.getClass()) || other.getClass().isAssignableFrom(this.getClass()) ;
+	}
 } 
--- src/type/TypeInteger.java
+++ src/type/TypeInteger.java
@@ -1,5 +1,6 @@
 package type;
 
+// public class TypeInteger extends TypeFloat { // TODO: Implement type conversions
 public class TypeInteger extends Type {
 	private static final String name = "int";
 
