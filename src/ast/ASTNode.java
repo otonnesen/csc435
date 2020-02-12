bda5ae174ee1c0dfa5113e554f6c9a4a7beac20e
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-11 21:15:36 -0800

    Added line/offset fields to ASTNode.

--- src/ast/ASTNode.java
+++ src/ast/ASTNode.java
@@ -3,5 +3,24 @@ import visitor.Visitor;
 import visitor.Visitable;
 
 public abstract class ASTNode implements Visitable {
+	private int line;
+	private int offset;
+
+	public int getLine() {
+		return this.line;
+	}
+
+	public int getOffset() {
+		return this.offset;
+	}
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
 	public abstract Object accept(Visitor v);
 }
