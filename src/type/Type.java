622f8681dece78459b206cade187e274f51c0188
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-13 06:30:21 -0800

    Added super/subtype distinction.

--- src/type/Type.java
+++ src/type/Type.java
@@ -8,7 +8,16 @@ public abstract class Type {
 	}
 
 	public boolean comparable(Type other) {
-		return this.getClass().isAssignableFrom(other.getClass())
-			|| other.getClass().isAssignableFrom(this.getClass());
+		return this.subtype(other) || this.supertype(other);
 	}
-} 
+
+	public boolean subtype(Type other) {
+		// Returns true if this is a subtype of other
+		return other.getClass().isAssignableFrom(this.getClass());
+	}
+
+	public boolean supertype(Type other) {
+		// Returns true if this is a supertype of other
+		return this.getClass().isAssignableFrom(other.getClass());
+	}
+}
