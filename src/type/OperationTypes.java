9f1a93563c0e98ca96b1387721d2ba0679856619
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-13 09:31:58 -0800

    Moved operation type HashSets into a HashMap indexed by the operation string. Moved everything into a static initialiser.

--- src/type/OperationTypes.java
+++ src/type/OperationTypes.java
@@ -2,41 +2,59 @@ package type;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.HashMap;
+
+import ast.ExpressionIsEqual;
+import ast.ExpressionLessThan;
+import ast.ExpressionMinus;
+import ast.ExpressionPlus;
+import ast.ExpressionTimes;
 
 public final class OperationTypes {
-	// These are sets indicating which types can be used with which operations.
-	public static final HashSet<Type> minus = new HashSet<Type>(
-			Arrays.asList(
-				TypeInteger.getInstance(),
-				TypeFloat.getInstance(),
-				TypeCharacter.getInstance()
-			));
-	public static final HashSet<Type> plus = new HashSet<Type>(
-			Arrays.asList(
-				TypeInteger.getInstance(),
-				TypeFloat.getInstance(),
-				TypeCharacter.getInstance(),
-				TypeString.getInstance()
-			));
-	public static final HashSet<Type> times = new HashSet<Type>(
-			Arrays.asList(
-				TypeInteger.getInstance(),
-				TypeFloat.getInstance()
-			));
-	public static final HashSet<Type> lessThan = new HashSet<Type>(
-			Arrays.asList(
-				TypeInteger.getInstance(),
-				TypeFloat.getInstance(),
-				TypeCharacter.getInstance(),
-				TypeString.getInstance(),
-				TypeBoolean.getInstance()
-			));
-	public static final HashSet<Type> equals = new HashSet<Type>(
-			Arrays.asList(
-				TypeInteger.getInstance(),
-				TypeFloat.getInstance(),
-				TypeCharacter.getInstance(),
-				TypeString.getInstance(),
-				TypeBoolean.getInstance()
-			));
+	public static final HashMap<String, HashSet<Type>> opTypes;
+	static {
+		// These are sets indicating which types can be used with which
+		// operations.
+		HashSet<Type> equals = new HashSet<Type>(
+				Arrays.asList(
+					TypeInteger.getInstance(),
+					TypeFloat.getInstance(),
+					TypeCharacter.getInstance(),
+					TypeString.getInstance(),
+					TypeBoolean.getInstance()
+				));
+		HashSet<Type> lessThan = new HashSet<Type>(
+				Arrays.asList(
+					TypeInteger.getInstance(),
+					TypeFloat.getInstance(),
+					TypeCharacter.getInstance(),
+					TypeString.getInstance(),
+					TypeBoolean.getInstance()
+				));
+		HashSet<Type> minus = new HashSet<Type>(
+				Arrays.asList(
+					TypeInteger.getInstance(),
+					TypeFloat.getInstance(),
+					TypeCharacter.getInstance()
+				));
+		HashSet<Type> plus = new HashSet<Type>(
+				Arrays.asList(
+					TypeInteger.getInstance(),
+					TypeFloat.getInstance(),
+					TypeCharacter.getInstance(),
+					TypeString.getInstance()
+				));
+		HashSet<Type> times = new HashSet<Type>(
+				Arrays.asList(
+					TypeInteger.getInstance(),
+					TypeFloat.getInstance()
+				));
+
+		opTypes = new HashMap<String, HashSet<Type>>();
+		opTypes.put(ExpressionIsEqual.op, equals);
+		opTypes.put(ExpressionLessThan.op, lessThan);
+		opTypes.put(ExpressionMinus.op, minus);
+		opTypes.put(ExpressionPlus.op, plus);
+		opTypes.put(ExpressionTimes.op, times);
+	}
 }
