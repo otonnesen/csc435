63d8ef7f45dbfd4e533b7a9ef462ee64de969dca
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-13 09:30:23 -0800

    Added static operation string fields to ExpressionOperation subclasses for access from outside ast package.

--- src/ast/ExpressionLessThan.java
+++ src/ast/ExpressionLessThan.java
@@ -3,8 +3,10 @@ package ast;
 import visitor.Visitor;
 
 public class ExpressionLessThan extends ExpressionOperation {
+	public static final String op = "<";
+
 	public ExpressionLessThan(Expression left, Expression right) {
-		super(left, right, "<");
+		super(left, right, op);
 	}
 
 	public <T> T accept(Visitor<T> v) {
--- src/ast/ExpressionPlus.java
+++ src/ast/ExpressionPlus.java
@@ -3,8 +3,10 @@ package ast;
 import visitor.Visitor;
 
 public class ExpressionPlus extends ExpressionOperation {
+	public static final String op = "+";
+
 	public ExpressionPlus(Expression left, Expression right) {
-		super(left, right, "+");
+		super(left, right, op);
 	}
 
 	public <T> T accept(Visitor<T> v) {
