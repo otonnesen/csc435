52889c3b205b371701d62ecfb9e0cc792dfe37ed
Oliver Tonnesen <oliver@tonnesen.com>
2020-02-27 15:19:34 -0800

    Created skeleton for IRVisitor.

---
+++ src/visitor/IRVisitor.java
@@ -0,0 +1,97 @@
+package visitor;
+
+import ir.*;
+import ast.*;
+
+public class IRVisitor extends Visitor<Temp> {
+	public Temp visit(Block b) {
+		return null;
+	}
+	public Temp visit(Declaration d) {
+		return null;
+	}
+	public Temp visit(ExpressionArrayAccess e) {
+		return null;
+	}
+	public Temp visit(ExpressionFunctionCall e) {
+		return null;
+	}
+	public Temp visit(ExpressionIdentifier e) {
+		return null;
+	}
+	public Temp visit(ExpressionIsEqual e) {
+		return null;
+	}
+	public Temp visit(ExpressionLessThan e) {
+		return null;
+	}
+	public Temp visit(ExpressionMinus e) {
+		return null;
+	}
+	public Temp visit(ExpressionParenthesis e) {
+		return null;
+	}
+	public Temp visit(ExpressionPlus e) {
+		return null;
+	}
+	public Temp visit(ExpressionTimes e) {
+		return null;
+	}
+	public Temp visit(FunctionBody fb) {
+		return null;
+	}
+	public Temp visit(FunctionDeclaration fd) {
+		return null;
+	}
+	public Temp visit(Function f) {
+		return null;
+	}
+	public Temp visit(LiteralBoolean b) {
+		return null;
+	}
+	public Temp visit(LiteralCharacter c) {
+		return null;
+	}
+	public Temp visit(LiteralFloat f) {
+		return null;
+	}
+	public Temp visit(LiteralInteger i) {
+		return null;
+	}
+	public Temp visit(LiteralString s) {
+		return null;
+	}
+	public Temp visit(Program p) {
+		return null;
+	}
+	public Temp visit(StatementArrayAssignment s) {
+		return null;
+	}
+	public Temp visit(StatementAssign s) {
+		return null;
+	}
+	public Temp visit(StatementEmpty s) {
+		return null;
+	}
+	public Temp visit(StatementExpression s) {
+		return null;
+	}
+	public Temp visit(StatementIf s) {
+		return null;
+	}
+	public Temp visit(StatementPrint s) {
+		return null;
+	}
+	public Temp visit(StatementPrintln s) {
+		return null;
+	}
+	public Temp visit(StatementReturn s) {
+		return null;
+	}
+	public Temp visit(StatementWhile s) {
+		return null;
+	}
+	public Temp visit(VariableDeclaration v) {
+		return null;
+	}
+}
