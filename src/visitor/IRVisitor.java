ec2609410d91e7318660ca655c740f1bc04be280
Oliver Tonnesen <oliver@tonnesen.com>
2020-03-01 20:32:37 -0800

    Updted IRVisitor skeleton to resolve class name conflicts between ast and ir.

--- src/visitor/IRVisitor.java
+++ src/visitor/IRVisitor.java
@@ -1,97 +1,97 @@
 package visitor;
 
 import ir.*;
-import ast.*;
+import type.Type;
 
 public class IRVisitor extends Visitor<Temp> {
-	public Temp visit(Block b) {
+	public Temp visit(ast.Block b) {
 		return null;
 	}
-	public Temp visit(Declaration d) {
+	public Temp visit(ast.Declaration d) {
 		return null;
 	}
-	public Temp visit(ExpressionArrayAccess e) {
+	public Temp visit(ast.ExpressionArrayAccess e) {
 		return null;
 	}
-	public Temp visit(ExpressionFunctionCall e) {
+	public Temp visit(ast.ExpressionFunctionCall e) {
 		return null;
 	}
-	public Temp visit(ExpressionIdentifier e) {
+	public Temp visit(ast.ExpressionIdentifier e) {
 		return null;
 	}
-	public Temp visit(ExpressionIsEqual e) {
+	public Temp visit(ast.ExpressionIsEqual e) {
 		return null;
 	}
-	public Temp visit(ExpressionLessThan e) {
+	public Temp visit(ast.ExpressionLessThan e) {
 		return null;
 	}
-	public Temp visit(ExpressionMinus e) {
+	public Temp visit(ast.ExpressionMinus e) {
 		return null;
 	}
-	public Temp visit(ExpressionParenthesis e) {
+	public Temp visit(ast.ExpressionParenthesis e) {
 		return null;
 	}
-	public Temp visit(ExpressionPlus e) {
+	public Temp visit(ast.ExpressionPlus e) {
 		return null;
 	}
-	public Temp visit(ExpressionTimes e) {
+	public Temp visit(ast.ExpressionTimes e) {
 		return null;
 	}
-	public Temp visit(FunctionBody fb) {
+	public Temp visit(ast.FunctionBody fb) {
 		return null;
 	}
-	public Temp visit(FunctionDeclaration fd) {
+	public Temp visit(ast.FunctionDeclaration fd) {
 		return null;
 	}
-	public Temp visit(Function f) {
+	public Temp visit(ast.Function f) {
 		return null;
 	}
-	public Temp visit(LiteralBoolean b) {
+	public Temp visit(ast.LiteralBoolean b) {
 		return null;
 	}
-	public Temp visit(LiteralCharacter c) {
+	public Temp visit(ast.LiteralCharacter c) {
 		return null;
 	}
-	public Temp visit(LiteralFloat f) {
+	public Temp visit(ast.LiteralFloat f) {
 		return null;
 	}
-	public Temp visit(LiteralInteger i) {
+	public Temp visit(ast.LiteralInteger i) {
 		return null;
 	}
-	public Temp visit(LiteralString s) {
+	public Temp visit(ast.LiteralString s) {
 		return null;
 	}
-	public Temp visit(Program p) {
+	public Temp visit(ast.Program p) {
 		return null;
 	}
-	public Temp visit(StatementArrayAssignment s) {
+	public Temp visit(ast.StatementArrayAssignment s) {
 		return null;
 	}
-	public Temp visit(StatementAssign s) {
+	public Temp visit(ast.StatementAssign s) {
 		return null;
 	}
-	public Temp visit(StatementEmpty s) {
+	public Temp visit(ast.StatementEmpty s) {
 		return null;
 	}
-	public Temp visit(StatementExpression s) {
+	public Temp visit(ast.StatementExpression s) {
 		return null;
 	}
-	public Temp visit(StatementIf s) {
+	public Temp visit(ast.StatementIf s) {
 		return null;
 	}
-	public Temp visit(StatementPrint s) {
+	public Temp visit(ast.StatementPrint s) {
 		return null;
 	}
-	public Temp visit(StatementPrintln s) {
+	public Temp visit(ast.StatementPrintln s) {
 		return null;
 	}
-	public Temp visit(StatementReturn s) {
+	public Temp visit(ast.StatementReturn s) {
 		return null;
 	}
-	public Temp visit(StatementWhile s) {
+	public Temp visit(ast.StatementWhile s) {
 		return null;
 	}
-	public Temp visit(VariableDeclaration v) {
+	public Temp visit(ast.VariableDeclaration v) {
 		return null;
 	}
 }
